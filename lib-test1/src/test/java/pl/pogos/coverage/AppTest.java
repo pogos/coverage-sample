d85f39ef7933eebd37a8953910a6e00e511feb5a
pogos <seba.pogo@gmail.com>
2020-05-21 12:27:07 +0200

    Initialize project - groovy, spock, junit5

---
+++ lib-test1/src/test/java/pl/pogos/coverage/AppTest.java
@@ -0,0 +1,14 @@
+package pl.pogos.coverage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
